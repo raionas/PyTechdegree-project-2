aadb6e9847bdd2ec79748e0100a6257d522d3412
raionas <rmpherradura@gmail.com>
2019-05-14 14:57:14 +1200

    Update app.py

--- basketball-team-stats_v1/app.py
+++ basketball-team-stats_v1/app.py
@@ -76,8 +76,26 @@ def distribute_players(headcount):
         Warriors = skilled[6:9] + not_skilled[6:9]
     return Panthers, Bandits, Warriors
 
-def stats_menu(preferred):
-    choice = main_selection()
+def display_stats(team):
+    roster = []
+    seasoned = []
+    not_seasoned = []
+    keepers = []
+    Panthers, Bandits, Warriors = distribute_players(players_list)
+    teams = Panthers, Bandits, Warriors
+
+    for items in teams:
+        for squad in items:
+            roster.append(squad["name"])
+            if squad["experience"] == True:
+                seasoned.append(squad["name"])
+            if squad["experience"] == False:
+                not_seasoned.append(squad["name"])
+                keepers.extend(squad["guardians"])
+
+            sum_ht = sum(squad["height"] for squad in items)
+            avg_ht = int(sum_ht / len(roster))
+
     stats_template = dedent("""\
                      Team: {} Stats
                      {}
@@ -85,98 +103,125 @@ def stats_menu(preferred):
                      Players on Team: {}
                        Total Experienced Players: {}
                        Total Inexperienced Players: {}
-                         The average height for the Bandits Team is: {}
-                         The Panthers Guardians: {} """)
+                         The average height for the team is: {}
+                         The Guardians: {} """)
+    print(stats_template.format(
+                "Warriors",
+                "-" * len("Team: Warriors Stats"),
+                len(teams),
+                ", ".join(roster),
+                len(seasoned),
+                len(not_seasoned),
+                avg_ht,
+                ", ".join(keepers)))
+    return teams
 
-    # skilled, not_skilled = divide_experienced(players_list)
-    # if len(skilled) % len(not_skilled) == 0:
-    #     Panthers = skilled[0:3] + not_skilled[0:3]
-    #     Bandits = skilled[3:6] + not_skilled[3:6]
-    #     Warriors = skilled[6:9] + not_skilled[6:9]
-    Panthers, Bandits, Warriors = distribute_players(players_list)
-
+def stats_menu(preferred):
+    choice = main_selection()
     if choice == 1:
-        roster1 = []
-        seasoned_panthers = []
-        not_seasoned_panthers = []
-        panthers_keepers = []
-
-        for items in Panthers:
-            roster1.append(items["name"])
-            if items["experience"] == True:
-                seasoned_panthers.append(items["name"])
-            if items["experience"] == False:
-                not_seasoned_panthers.append(items["name"])
-            panthers_keepers.extend(items["guardians"])
-
-        panthers_sum_ht = sum(items["height"] for items in Panthers)
-        panthers_avg_ht = int(panthers_sum_ht / len(roster1))
-
-        print(stats_template.format(
-                    "Panthers",
-                    "-" * len("Team: Panthers Stats"),
-                    len(Warriors),
-                    ", ".join(roster1),
-                    len(seasoned_panthers),
-                    len(not_seasoned_panthers),
-                    panthers_avg_ht,
-                    ", ".join(panthers_keepers)))
-
-    elif choice == 2:
-        roster2 = []
-        seasoned_bandits = []
-        not_seasoned_bandits = []
-        bandits_keepers = []
-
-        for items in Bandits:
-            roster2.append(items["name"])
-            if items["experience"] == True:
-                seasoned_bandits.append(items["name"])
-            if items["experience"] == False:
-                not_seasoned_bandits.append(items["name"])
-            bandits_keepers.extend(items["guardians"])
-
-        bandits_sum_ht = sum(items["height"] for items in Bandits)
-        bandits_avg_ht = int(bandits_sum_ht / len(roster2))
-
-        print(stats_template.format(
-                    "Bandits",
-                    "-" * len("Team: Bandits Stats"),
-                    len(Bandits),
-                    ", ".join(roster2),
-                    len(seasoned_bandits),
-                    len(not_seasoned_bandits),
-                    bandits_avg_ht,
-                    ", ".join(bandits_keepers)))
-
-    elif choice == 3:
-        roster3 = []
-        seasoned_warriors = []
-        not_seasoned_warriors = []
-        warriors_keepers = []
-
-        for items in Warriors:
-            roster3.append(items["name"])
-            if items["experience"] == True:
-                seasoned_warriors.append(items["name"])
-            if items["experience"] == False:
-                not_seasoned_warriors.append(items["name"])
-            warriors_keepers.extend(items["guardians"])
-
-        warriors_sum_ht = sum(items["height"] for items in Warriors)
-        warriors_avg_ht = int(warriors_sum_ht / len(roster3))
-
-        print(stats_template.format(
-                    "Warriors",
-                    "-" * len("Team: Warriors Stats"),
-                    len(Warriors),
-                    ", ".join(roster3),
-                    len(seasoned_warriors),
-                    len(not_seasoned_warriors),
-                    warriors_avg_ht,
-                    ", ".join(warriors_keepers)))
-
+        display_stats("Panthers")
+    if choice == 2:
+        display_stats("Bandits")
+    if choice == 3:
+        display_stats("Warriors")
     return choice
+
+# def stats_menu(preferred):
+#     choice = main_selection()
+#     stats_template = dedent("""\
+#                      Team: {} Stats
+#                      {}
+#                      Total Players: {}
+#                      Players on Team: {}
+#                        Total Experienced Players: {}
+#                        Total Inexperienced Players: {}
+#                          The average height for the Bandits Team is: {}
+#                          The Panthers Guardians: {} """)
+
+#     Panthers, Bandits, Warriors = distribute_players(players_list)
+#
+#     if choice == 1:
+#         roster1 = []
+#         seasoned_panthers = []
+#         not_seasoned_panthers = []
+#         panthers_keepers = []
+#
+#         for items in Panthers:
+#             roster1.append(items["name"])
+#             if items["experience"] == True:
+#                 seasoned_panthers.append(items["name"])
+#             if items["experience"] == False:
+#                 not_seasoned_panthers.append(items["name"])
+#             panthers_keepers.extend(items["guardians"])
+#
+#         panthers_sum_ht = sum(items["height"] for items in Panthers)
+#         panthers_avg_ht = int(panthers_sum_ht / len(roster1))
+#
+#         print(stats_template.format(
+#                     "Panthers",
+#                     "-" * len("Team: Panthers Stats"),
+#                     len(Warriors),
+#                     ", ".join(roster1),
+#                     len(seasoned_panthers),
+#                     len(not_seasoned_panthers),
+#                     panthers_avg_ht,
+#                     ", ".join(panthers_keepers)))
+#
+#     elif choice == 2:
+#         roster2 = []
+#         seasoned_bandits = []
+#         not_seasoned_bandits = []
+#         bandits_keepers = []
+#
+#         for items in Bandits:
+#             roster2.append(items["name"])
+#             if items["experience"] == True:
+#                 seasoned_bandits.append(items["name"])
+#             if items["experience"] == False:
+#                 not_seasoned_bandits.append(items["name"])
+#             bandits_keepers.extend(items["guardians"])
+#
+#         bandits_sum_ht = sum(items["height"] for items in Bandits)
+#         bandits_avg_ht = int(bandits_sum_ht / len(roster2))
+#
+#         print(stats_template.format(
+#                     "Bandits",
+#                     "-" * len("Team: Bandits Stats"),
+#                     len(Bandits),
+#                     ", ".join(roster2),
+#                     len(seasoned_bandits),
+#                     len(not_seasoned_bandits),
+#                     bandits_avg_ht,
+#                     ", ".join(bandits_keepers)))
+#
+#     elif choice == 3:
+#         roster3 = []
+#         seasoned_warriors = []
+#         not_seasoned_warriors = []
+#         warriors_keepers = []
+#
+#         for items in Warriors:
+#             roster3.append(items["name"])
+#             if items["experience"] == True:
+#                 seasoned_warriors.append(items["name"])
+#             if items["experience"] == False:
+#                 not_seasoned_warriors.append(items["name"])
+#             warriors_keepers.extend(items["guardians"])
+#
+#         warriors_sum_ht = sum(items["height"] for items in Warriors)
+#         warriors_avg_ht = int(warriors_sum_ht / len(roster3))
+#
+        # print(stats_template.format(
+        #             "Warriors",
+        #             "-" * len("Team: Warriors Stats"),
+        #             len(Warriors),
+        #             ", ".join(roster3),
+        #             len(seasoned_warriors),
+        #             len(not_seasoned_warriors),
+        #             warriors_avg_ht,
+        #             ", ".join(warriors_keepers)))
+#
+#     return choice
 
 if __name__ == "__main__":
     #clean_data, divide_experience, main_menu, stats_menu, distribute_players, print_stats
@@ -187,4 +232,4 @@ if __name__ == "__main__":
             stats_menu(1)
             input("\nPress ENTER to continue...")
     except SystemExit:
-        print("Program Terminated. Bye!")
+        print("Program Terminated, Bye!")
